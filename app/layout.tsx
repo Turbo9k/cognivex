897a3db36353a6035f3b8680c66ae64bc7d7f2d6
Turbo9k <iansiats26@gmail.com>
2025-06-24 13:07:17 -0600

    Remove duplicate header - keep only Navigation component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navigation from '@/components/layout/Navigation'
 import { ThemeProvider } from '@/components/theme-provider'
-import Header from '@/components/layout/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,7 +31,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
           <Navigation />
           <main>{children}</main>
         </ThemeProvider>
